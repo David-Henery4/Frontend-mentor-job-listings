af883804f7e1fb52de9578ca7ac0161aca49f626
DizzlyD <david4henery00@hotmail.com>
2022-05-14 22:51:29 +0100

    initial commit

---
+++ J.s/main.js
@@ -0,0 +1,3 @@
+import * as control from "./mvc/controller/control.js";
+
+console.log("working")
---
+++ J.s/mvc/controller/control.js
@@ -0,0 +1,8 @@
+// Control
+import * as model from "../model/model.js";
+import * as view from "../views/view.js"; // might not need
+import * as jobsView from "../views/jobsView.js";
+
+
+console.log("control working")
+
